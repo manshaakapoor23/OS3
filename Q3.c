17f0adee38c764a75ae0fa1b238daf739e806d57
manshaakapoor23 <108448947+manshaakapoor23@users.noreply.github.com>
2023-11-24 00:12:38 +0530

    Add files via upload

---
+++ Q3.c
@@ -0,0 +1,15 @@
+void* left(void* args){
+
+}                   // cars on the left
+
+void* right(void* args){
+
+}               // cars on the right
+
+void passing(int dir){
+
+}   
+
+int main{
+    
+}
